4d7dcff67e367728e4465eb12d0b1238dcd3d472
Raiam-Dev2 <dinheiroteste989@gmail.com>
2025-08-24 09:22:47 -0300

    Ajuste

---
+++ Automacao/Features/TesteController.cs
@@ -0,0 +1,15 @@
+﻿using Automacao.Features.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Automacao.Features
+{
+    [Route("api/[controller]")]
+    public class TesteController : ApiController
+    {
+        [HttpGet("lancar")]
+        public IActionResult Pegar()
+        {
+            return ResponseOk();
+        }
+    }
+}
